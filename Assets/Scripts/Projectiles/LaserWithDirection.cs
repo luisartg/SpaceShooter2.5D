ce7adfac60087140037e4cf64098a92585b6661e
Luis Arturo Guerra Garcia <luis.art.guerra@gmail.com>
2020-02-09 10:28:23 -0600

    Created a new Laser movement (LaserWithDirection)
    You can define the laser direction with SetDirection function.
    
    Also, moved all related scripts for lasers to folder "Projectiles"

---
+++ Assets/Scripts/Projectiles/LaserWithDirection.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserWithDirection : MonoBehaviour
+{
+    [SerializeField] float _speed = 8f;
+    [SerializeField] float _topLimit = 8f;
+    [SerializeField] float _bottomLimit = -8f;
+    [SerializeField] float _leftLimit = -11f;
+    [SerializeField] float _rightLimit = 11f;
+    [SerializeField] Vector2 _direction = Vector2.down;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        DestroyIfOutOfBounds();
+    }
+
+    private void Move()
+    {
+        transform.Translate(_direction * _speed * Time.deltaTime);
+    }
+
+    private void DestroyIfOutOfBounds()
+    {
+        if (IsOutOfBounds())
+        {
+            DestroyParentContainer();
+            Destroy(gameObject);
+        }
+    }
+
+    private bool IsOutOfBounds()
+    {
+        return transform.position.y > _topLimit || 
+               transform.position.y < _bottomLimit ||
+               transform.position.x > _rightLimit ||
+               transform.position.x < _leftLimit;
+    }
+
+    private void DestroyParentContainer()
+    {
+        if (transform.parent != null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+    public void SetDirection(Vector2 newDirection)
+    {
+        _direction = newDirection;
+    }
+}
